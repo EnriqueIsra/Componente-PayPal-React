2509d54303685e0e3b468d922dfe0935ccc5599a
EnriqueIsra <sirilo99x3@gmail.com>
2022-10-30 12:37:28 -0600

    commit

--- paypal_checkout/src/App.js
+++ paypal_checkout/src/App.js
@@ -31,7 +31,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
         <paypalCheckoutButton order={order}/>
       </header>
